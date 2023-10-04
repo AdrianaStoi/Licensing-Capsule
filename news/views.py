30355eaf17621871eed79c14b743676cced65664
AdrianaStoi <>
2023-10-04 15:30:37 +0000

    Create single article view and html structure template

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Article
 
 
@@ -17,3 +17,24 @@ class ListArticle(generic.ListView):
     template_name = 'news.html' 
     context_object_name = 'all_articles'
     paginate_by = 5
+
+class SingleArticle(View):
+
+
+    def get (self, request, slug, *args, **kwargs):
+        queryset = Article.objects.filter(status=1)
+        article = get_object_or_404(queryset, slug=slug)
+        comments = article.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if article.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "singlearticle.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
