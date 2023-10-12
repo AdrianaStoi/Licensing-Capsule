4e22f5a9f5b7b236bea86dd318f491f86ee01f30
AdrianaStoi <>
2023-10-12 21:20:04 +0000

    Add modal and onclick event to like icon when user is not logged in

---
+++ static/modal.js
@@ -0,0 +1,11 @@
+
+function showModal() {
+    const modal = document.getElementById("login-modal");
+    modal.style.display = "block";
+
+}
+
+function closeModal() {
+    const modal = document.getElementById("login-modal");
+    modal.style.display = "none";
+}
