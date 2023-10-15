104cf9e90ac413f1fca3308b039a43261ea4fd19
AdrianaStoi <>
2023-10-15 14:17:16 +0000

    Add message alert when user logs in and logs out

--- static/modal.js
+++ static/modal.js
@@ -9,3 +9,10 @@ function closeModal() {
     const modal = document.getElementById("login-modal");
     modal.style.display = "none";
 }
+
+setTimeout(function () {
+    let messages = document.getElementById('message-timeout');
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 3500);
+
