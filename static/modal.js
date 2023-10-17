2a2742e11d2d217bb2edc615c3d5ae07b7e15f86
AdrianaStoi <>
2023-10-17 20:49:00 +0000

    Add comments to static js and css

--- static/modal.js
+++ static/modal.js
@@ -1,18 +1,30 @@
 
+/**
+ * Function to display the login modal 
+ * by setting its style property to "block"
+ */
 function showModal() {
     const modal = document.getElementById("login-modal");
     modal.style.display = "block";
 
 }
 
+/**
+ * Function to hide the login modal 
+ * by setting its style property to "none"
+ */
 function closeModal() {
     const modal = document.getElementById("login-modal");
     modal.style.display = "none";
 }
 
+/**
+ * Set timeout for 5 seconds
+ * to automatically close a bootstrap alert message
+ */
 setTimeout(function () {
     let messages = document.getElementById('message-timeout');
     let alert = new bootstrap.Alert(messages);
     alert.close();
-}, 3500);
+}, 5000);
 
